a91113bea898b5f22d5eb8f05f490f4777ae566f
Saiful-Bahri-95 <saiful.bahri2004@gmail.com>
2025-06-28 12:21:13 +0700

    update media android

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -3,22 +3,18 @@
 var sectionArray = [1, 2, 3, 4, 5];
 
 $.each(sectionArray, function(index, value){
-          
-     $(document).scroll(function(){
-         var offsetSection = $('#' + 'section_' + value).offset().top - 90;
-         var docScroll = $(document).scrollTop();
-         var docScroll1 = docScroll + 1;
-         
-        
-         if ( docScroll1 >= offsetSection ){
-             $('.navbar-nav .nav-item .nav-link').removeClass('active');
-             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
-             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
-             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
-         }
-         
-     });
-    
+    $(document).scroll(function(){
+        var offsetSection = $('#' + 'section_' + value).offset().top - 90;
+        var docScroll = $(document).scrollTop();
+        var docScroll1 = docScroll + 1;
+        if ( docScroll1 >= offsetSection ){
+            $('.navbar-nav .nav-item .nav-link').removeClass('active');
+            $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
+            $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+            $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+        }
+    });
+
     $('.click-scroll').eq(index).click(function(e){
         var offsetClick = $('#' + 'section_' + value).offset().top - 90;
         e.preventDefault();
@@ -26,7 +22,7 @@ $.each(sectionArray, function(index, value){
             'scrollTop':offsetClick
         }, 300)
     });
-    
+
 });
 
 $(document).ready(function(){
